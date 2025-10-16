6c39575b3a66e011d4142006236dbdad34f2a5c1
thatsarealstar <thatsarealstar@gmail.com>
2025-10-16 18:53:51 -0400

    Create SherlockINS bot script with event listeners
    Initial implementation of SherlockINS bot script with basic event handling.

---
+++ bot.js
@@ -0,0 +1,29 @@
+/* 
+SCRIPT INFO. 
+Name: SherlockINS Bot Script
+Description: A bot created by Sherlock to spice up boring lobbies or just for fun!
+Creator: thatsarealstar (Sherlock)
+Time: 6:44 PM (10/16/2025)
+
+Update Logs:
+[10/16/2025]: At 6:44 PM, the SherlockINS repository was made! The start of a fun and exciting bot to come.
+
+INFORMATION IS NOT REMOVABLE. DO NOT REMOVE ON ANY EXTENT UNLESS PERMITTED!!
+*/
+
+MPP.client.on("hi", () => {
+  console.log("Checking name and color configuration...")
+  if (MPP.client.name !== "👌 SherlockINS" && MPP.client.color !== "#ff0000") {
+    console.log("Name and color wrong! Setting...")
+    MPP.client.sendArray([{ m: userset, set: { name: "👌 SherlockINS", color: "#ff0000" }])
+  }
+})
+
+MPP.client.on("a", (msg) => {
+  const message = msg.a
+  const command = message[0]
+  if (command[0] === ";help") {
+    MPP.client.sendArray([{m: "a", message: "Heyo! The commands will be dm'd to you. (Use ;dmrules for information on why this is happening.)", reply_to: msg.id}])
+    MPP.client.sendArray([{m: "dm", message: "Thank you for using SherlockINS! Unfortunately, this version has absolutely no commands. Check again later!", _id: msg.p._id, reply_to: msg.id)]}
+  }
+})
